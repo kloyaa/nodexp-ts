3fdc61f4f2ad9cecd92be9e762e147b78bf00809
Kolya Madridano <kolya.madridano@gmail.com>
2023-05-09 21:29:42 +0800

    fix(deployment): update vercel.json

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 import useragent from 'express-useragent';
-import { getAwsSecrets } from "./src/__core/utils";
+import { getAwsSecrets } from "./__core/utils";
 const app = express();
 
 async function runApp() {
@@ -32,15 +32,16 @@ async function runApp() {
     app.use(useragent.express());
     app.use(express.json());
     app.use(express.urlencoded({ limit: '25mb', extended: true }));
-    app.use("/api/v1", require("./src/controllers/security.controller"));
-    app.use("/api/v1", require("./src/controllers/auth.controller"));
+    
+    app.get('/', (req, res) => res.send('Express Typescript on Vercel'));
+    app.use("/api/v1", require("./controllers/security.controller"));
+    app.use("/api/v1", require("./controllers/auth.controller"));
 
     app.listen(port, () => {
         console.log(`RUNNING ON ${process.env.NODE_ENV}`)
         console.log(`RUNNING ON ${port}`)
     });
 
-    return true;
 }
 
 runApp();
